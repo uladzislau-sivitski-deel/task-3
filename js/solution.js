1cb10427df9731fb163791c9cd1b661a5b61eafe
Vladislav Sivitsky <vladosiv@gmail.com>
2018-02-08 18:30:20 +0300

    no message

--- js/solution.js
+++ js/solution.js
@@ -100,7 +100,7 @@
     function splitArray(arr, chunkSize) {
         let chunks = [], i;
         for (i = 0; i < arr.length; i += chunkSize) {
-            groups.push(arr.slice(i, i + chunkSize));
+            chunks.push(arr.slice(i, i + chunkSize));
         }
         return chunks;
     }
